f7dd3597b40a05a4f13a6f662e07e47b991fcc64
sdman <zh268262441@126.com>
2019-11-30 17:02:55 +0800

    斐波那契数列以及青蛙跳台阶

---
+++ 9.py
@@ -0,0 +1,35 @@
+#-*-coding:utf-8 -*-
+
+import numpy as np
+#实现斐波那契数列的实用解法
+def Fibonacci(n):
+    if n<2:
+        return n
+    fn_1=1
+    fn_2=0
+    for i in range(2,n+1):
+        fn=fn_1+fn_2
+        fn_2=fn_1
+        fn_1=fn
+    return fn
+
+
+#青蛙跳台阶：一次跳1级或者2级，斐波那契数列的应用
+def jumpFloor(number):
+    if number==1:
+        return number
+    res=[0,1,2]
+    for i in range(3,number+1):
+        res.append(res[i-1]+res[i-2])
+    return res[-1]
+
+#变态跳台阶：一次可以跳任意多级，求青蛙上一个n级台阶的走法？
+def jumpFloorII(number):
+    #fn=fn_1+fn_2+...+f1 fn_1=fn_2+fn_3+...+f1 两式相减，得到fn=2fn_1,通过数学归纳法，fn=2^(n-1)
+    num=1
+    for i in range(2,number+1):
+        num=2*num
+    return num
+
+if __name__=='__main__':
+    print(Fibonacci(5))
